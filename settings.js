3672ee4e7f6c457eaa7b1a25e0a1db051ba239f7
Nayef-Abou-Tayoun <nayef.abou.tayoun@ibm.com>
2026-01-17 19:43:13 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,17 +1,16 @@
 module.exports = {
-    uiPort: process.env.PORT || 1880,
+  flowFile: "flows.json",
+  flowFilePretty: true,
 
-    // 👇 IMPORTANT FIX
-    flowFile: 'flow-1.json',
-
-    adminAuth: null,
-    functionGlobalContext: {},
-
-    logging: {
-        console: {
-            level: "info",
-            metrics: false,
-            audit: false
-        }
+  editorTheme: {
+    projects: {
+      enabled: false
     }
+  },
+
+  runtimeState: {
+    enabled: false
+  },
+
+  disableEditor: true
 }
